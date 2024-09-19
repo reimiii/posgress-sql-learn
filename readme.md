43cb8b2edecdf551496e678188075627cdd3051c
Hilmi AM <imiia75775@proton.me>
2024-09-19 14:12:16 +0700

    init: hmm

---
+++ readme.md
@@ -0,0 +1,7 @@
+### postgressql
+berhubung pake docker jadi nya docker exec -it container psql --username=local --password
+
+
+buat ngeliat semua table `\l` di console
+
+buat database `create database name_db`
---
+++ test.sql
@@ -0,0 +1,16 @@
+-- buat db
+create database name_data;
+
+-- select all db
+select datname from pg_database;
+
+-- hapus db
+drop database name_data;
+
+-- buat db latihan
+create database latihan;
+
+-- pindah db klo di console `\c lathian` ga ada sql query buat pindah klo di dbclient
+
+
+
