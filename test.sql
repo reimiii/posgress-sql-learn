669647a242cbcc68271d7c16537df4acc073d3cb
Hilmi AM <imiia75775@proton.me>
2024-09-25 14:20:33 +0700

    join

--- test.sql
+++ test.sql
@@ -109,3 +109,79 @@ where code = 'P001';
 
 select *
 from products;
+
+drop table products;
+
+create table customers
+(
+    customer_id   serial       not null,
+    customer_name varchar(100) not null,
+    email         varchar(100) not null,
+    created_at    timestamp default current_timestamp,
+    primary key (customer_id),
+    constraint unique_email unique (email) -- constraint unique untuk email
+);
+
+create table products
+(
+    product_id   serial primary key,
+    product_name varchar(100)   not null,
+    price        numeric(10, 2) not null,                 -- harga harus lebih dari 0
+    stock        int            not null,                 -- stok tidak boleh negatif
+    created_at   timestamp default current_timestamp,
+    constraint unique_product_name unique (product_name), -- nama produk harus unik
+    constraint check_stock check ( stock >= 0 ),
+    constraint check_price check ( price > 0 )
+
+);
+
+
+create table wishlists
+(
+    wishlist_id serial not null, -- bisa juga: constraint pk_wishlist primary key (wishlist_id)
+    customer_id int    not null, -- menghubungkan ke tabel customers
+    product_id  int    not null, -- menghubungkan ke tabel products
+    added_at    timestamp default current_timestamp,
+
+    primary key (wishlist_id),
+    constraint fk_customer foreign key (customer_id) references customers (customer_id) on delete cascade,
+    constraint fk_product foreign key (product_id) references products (product_id) on delete cascade,
+    constraint unique_wishlist unique (customer_id, product_id)
+);
+
+insert into customers (customer_name, email)
+values ('Hilmi Akbar', 'hilmi.akbar@example.com'),
+       ('Dewi Suryani', 'dewi.suryani@example.com'),
+       ('Ahmad Fauzi', 'ahmad.fauzi@example.com'),
+       ('Budi Santoso', 'budi.santoso@example.com'),
+       ('Siti Aminah', 'siti.aminah@example.com');
+
+select *
+from customers;
+
+insert into products (product_name, price, stock)
+values ('Laptop Dell XPS', 15000000.00, 10),
+       ('Smartphone Samsung Galaxy', 7000000.00, 25),
+       ('Headphone Sony WH-1000XM4', 3500000.00, 15),
+       ('Mouse Logitech MX Master', 1200000.00, 50),
+       ('Monitor LG UltraWide', 5000000.00, 8);
+
+select *
+from products;
+
+insert into wishlists (customer_id, product_id)
+values (2, 3),
+       (2, 2);
+
+select *
+from wishlists;
+
+select count(w.wishlist_id) as total_wish, p.product_name, p.price
+from wishlists as w
+         join products p on p.product_id = w.product_id
+group by p.product_name, p.price;
+
+select count(w.product_id) as total_wish, p.product_name
+from wishlists as w
+         join products p on p.product_id = w.product_id
+group by p.product_name;
